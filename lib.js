b273e98e92664df535159b4f3510c8580e1a2356
Lincoln de Sousa <lincoln@comum.org>
2020-03-13 23:56:45 -0400

    feat: Render a pattern instead of just the dots

--- lib.js
+++ lib.js
@@ -2,7 +2,7 @@
    https://math.stackexchange.com/questions/2820194/how-to-plot-n-coords-to-distribute-evenly-as-a-ring-of-points-around-a-circle
 */
 
-function drawAllTheThings(canvas) {
+function drawAllTheThings(canvas, pattern) {
   const context = canvas.getContext('2d');
 
   const theme = {
@@ -12,13 +12,14 @@ function drawAllTheThings(canvas) {
     'outer-circle-radius': 80,
     'outer-circle-line-width': 2,
     'outer-circle-stroke-style': '#ccc',
-    'dot-on-radius': 2,
-    'dot-on-stroke-style': '#ccc',
-    'dot-on-fill-style': '#ccc',
+    'dot-path-stroke': '#000',
+    'dot-on-radius': 4,
+    'dot-on-stroke-style': '#000',
+    'dot-on-fill-style': '#000',
+    'dot-off-radius': 2,
+    'dot-off-stroke-style': '#ccc',
+    'dot-off-fill-style': '#ccc',
   };
-
-  /* Number of points that we want to plot */
-  const points = 12;
 
   /* Canvas dimensions */
   const [width, height] = [
@@ -31,7 +32,7 @@ function drawAllTheThings(canvas) {
 
   /* The entire circle is `360∘ ≡ 2π` and we we want to find the
      distance between each point `2π / N`. */
-  const angle = (2 * Math.PI) / points;
+  const angle = (2 * Math.PI) / pattern.length;
 
   /* This is where it all starts */
   const startAngle = -.5 * Math.PI;
@@ -55,28 +56,59 @@ function drawAllTheThings(canvas) {
     context.stroke();
   }
 
-  function drawDot(x, y) {
-    context.strokeStyle = theme['dot-on-stroke-style'];
-    context.fillStyle = theme['dot-on-fill-style'];
+  function drawDot(x, y, { radius, stroke, fill }) {
+    context.strokeStyle = stroke;
+    context.fillStyle = fill;
     context.beginPath();
-    context.arc(
-      x,
-      y,
-      theme['dot-on-radius'],
-      0,
-      2 * Math.PI,
-    );
+    context.arc(x, y, radius, 0, 2 * Math.PI);
     context.fill();
     context.stroke();
   }
 
+  function drawDotOn(x, y) {
+    drawDot(x, y, {
+      radius: theme['dot-on-radius'],
+      stroke: theme['dot-on-stroke-style'],
+      fill: theme['dot-on-fill-style'],
+    });
+  }
+
+  function drawDotOff(x, y) {
+    drawDot(x, y, {
+      radius: theme['dot-off-radius'],
+      stroke: theme['dot-off-stroke-style'],
+      fill: theme['dot-off-fill-style'],
+    });
+  }
+
   drawOuterCircle();
 
+  const patternDots = [];
+
+  function connectDots(dots) {
+    context.strokeStyle = theme['dot-path-stroke'];
+    context.beginPath();
+    context.moveTo(dots[0][0], dots[0][1]);
+    for (let i = 1; i < dots.length; i++) {
+      context.lineTo(dots[i][0], dots[i][1]);
+    }
+    context.closePath();
+    context.stroke();
+  }
+
   /* Find the equidistant points */
-  for (let i = 0; i < points; i++) {
+  for (let i = 0; i < pattern.length; i++) {
     const a = startAngle + angle * i;
     const x = Math.cos(a) * radius + width / 2;
     const y = Math.sin(a) * radius + height / 2;
-    drawDot(x, y);
+    if (pattern[i] === '1') {
+      drawDotOn(x, y);
+      patternDots.push([x, y]);
+    } else {
+      drawDotOff(x, y);
+    }
   }
+
+  /* Connect the dots */
+  connectDots(patternDots);
 }
